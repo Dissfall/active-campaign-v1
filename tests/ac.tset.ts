455ef2b01da7e1361192298e3167c76a0e2e3cb3
dissfall <dissfalling@gmail.com>
2020-02-11 14:05:11 +0300

    refactor: sepatare module and API tests

--- tests/ac.tset.ts
+++ tests/ac.tset.ts
@@ -1,28 +1,12 @@
 import { AC } from '../src/index'
-import { AccountViewRes } from '../src/types'
-import { name, internet, phone } from 'faker'
+import { lorem } from 'faker'
 
 let ac: AC
 
-// TODO: Skip API tests if no authorization data provided
-let AC_NAME: string
-let AC_TOKEN: string
+const AC_NAME = lorem.word()
+const AC_TOKEN = lorem.word()
 
 beforeAll(() => {
-  if (process.env.AC_NAME != undefined) {
-    AC_NAME = process.env.AC_NAME
-  } else {
-    console.error('AC_NAME environment variable required to run tests')
-    process.exit(1)
-  }
-
-  if (process.env.AC_TOKEN != undefined) {
-    AC_TOKEN = process.env.AC_TOKEN
-  } else {
-    console.error('AC_TOKEN environment variable required to run tests')
-    process.exit(1)
-  }
-
   ac = new AC(AC_NAME, AC_TOKEN)
 })
 
@@ -85,71 +69,5 @@ describe('Request preparing test', () => {
     expect(ac.request(action).format.serialize.url).toBe(
       `http://${AC_NAME}.api-us1.com/admin/api.php?api_action=${action}&api_output=serialize`
     )
-  })
-})
-
-describe('API test', () => {
-  it('Account View', () => {
-    jest.setTimeout(30000)
-    return ac.accountView().then((res: AccountViewRes) => {
-      expect(res.body.result_code).toBe(1)
-    })
-  })
-
-  it('Contact add', () => {
-    jest.setTimeout(30000)
-    return ac
-      .contactAdd({
-        email: internet.email(),
-        firstName: name.firstName(),
-        lastName: name.lastName()
-      })
-      .then((res: AccountViewRes) => {
-        expect(res.body.result_code).toBe(1)
-      })
-  })
-
-  it('Contact add without data', () => {
-    jest.setTimeout(30000)
-    return ac
-      .contactAdd({
-        email: internet.email()
-      })
-      .then((res: AccountViewRes) => {
-        expect(res.body.result_code).toBe(1)
-      })
-  })
-
-  // TEST field required
-  it('Contact add with field data', () => {
-    jest.setTimeout(30000)
-    return ac
-      .contactAdd({
-        email: internet.email(),
-        firstName: name.firstName(),
-        lastName: name.lastName(),
-        fields: {
-          TEST: 'TEST'
-        }
-      })
-      .then((res: AccountViewRes) => {
-        //TODO: More params to check
-        expect(res.body.result_code).toBe(1)
-      })
-  })
-
-  it('Contact add to list', () => {
-    jest.setTimeout(30000)
-    return ac
-      .contactAdd({
-        email: internet.email(),
-        firstName: name.firstName(),
-        lastName: name.lastName(),
-        list: 1
-      })
-      .then((res: AccountViewRes) => {
-        //TODO: More params to check
-        expect(res.body.result_code).toBe(1)
-      })
   })
 })
